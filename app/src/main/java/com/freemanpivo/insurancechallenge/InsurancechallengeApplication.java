780734f6ee6d2ac28440ff24f296d28caad8b56c
freemanpivo <andradepedroivo@gmail.com>
2024-04-27 18:13:12 -0300

    :tada: initial commit

---
+++ app/src/main/java/com/freemanpivo/insurancechallenge/InsurancechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.freemanpivo.insurancechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancechallengeApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/freemanpivo/insurancechallenge/InsurancechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemanpivo.insurancechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
