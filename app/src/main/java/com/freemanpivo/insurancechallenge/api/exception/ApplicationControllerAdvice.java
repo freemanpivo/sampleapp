a9e3eb84184e0d95187b420df8ab63341193c4fe
freemanpivo <andradepedroivo@gmail.com>
2024-04-28 14:16:20 -0300

    :hammer: add logs validation issues

--- app/src/main/java/com/freemanpivo/insurancechallenge/api/exception/ApplicationControllerAdvice.java
+++ app/src/main/java/com/freemanpivo/insurancechallenge/api/exception/ApplicationControllerAdvice.java
@@ -1,6 +1,10 @@
 package com.freemanpivo.insurancechallenge.api.exception;
 
+import com.freemanpivo.insurancechallenge.api.controller.ProductController;
 import com.freemanpivo.insurancechallenge.core.exception.RequestValidationException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,12 +13,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationControllerAdvice.class);
+
     @ExceptionHandler(RequestValidationException.class)
-    public ProblemDetail handleRequestValidation(RequestValidationException exception) {
+    public ProblemDetail handleRequestValidation(HttpServletRequest request, RequestValidationException exception) {
+        logger.info("there was validation issue with request in endpoint {}", request.getRequestURI());
         final var response = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
         response.setTitle("The request body sent is wrong");
         response.setDetail(exception.getMessage());
         response.setProperty("messages", exception.issues());
+        logger.info("end {} {}", request.getMethod(), request.getRequestURI());
 
         return response;
     }
--- app/src/test/java/com/freemanpivo/insurancechallenge/api/exception/ApplicationControllerAdviceTest.java
+++ app/src/test/java/com/freemanpivo/insurancechallenge/api/exception/ApplicationControllerAdviceTest.java
@@ -1,10 +1,12 @@
 package com.freemanpivo.insurancechallenge.api.exception;
 
 import com.freemanpivo.insurancechallenge.core.exception.RequestValidationException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 import java.util.List;
 
@@ -13,6 +15,7 @@ class ApplicationControllerAdviceTest {
 
     @Test
     public void testValidationException() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
         final var exception = new RequestValidationException(List.of(), "error message");
 
         final var expectedProblem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
@@ -20,7 +23,7 @@ class ApplicationControllerAdviceTest {
         expectedProblem.setDetail(exception.getMessage());
         expectedProblem.setProperty("messages", exception.issues());
 
-        Assertions.assertEquals(expectedProblem, handler.handleRequestValidation(exception));
+        Assertions.assertEquals(expectedProblem, handler.handleRequestValidation(request, exception));
     }
 
 }
